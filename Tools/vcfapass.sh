279e72d9e10ba3935604d40cabc5f07d936d249b
Burke Azbill <burkeazbill-bc@users.noreply.github.com>
2026-02-17 07:56:33 -0500

    Improve startup for 91

--- Tools/vcfapass.sh
+++ Tools/vcfapass.sh
@@ -2,18 +2,19 @@
 # HOL Usage: vcfapass.sh $(cat ~/creds.txt) $(cat ~/NEWPASSWORD.txt)
 #     Note: NEWPASSWORD.txt is created in the labstartup.sh by calling the holpwgen.sh script
 set timeout 20
-set old_password [lindex $argv 0]
-set new_password [lindex $argv 1]
+set host [lindex $argv 0]
+set old_password [lindex $argv 1]
+set new_password [lindex $argv 2]
 
-spawn ssh vmware-system-user@10.1.1.71
-expect "(vmware-system-user@10.1.1.71) Password:"
+spawn ssh vmware-system-user@$host
+expect "(vmware-system-user@$host) Password:"
 send "$old_password\r"
 expect "You are required to change your password immediately (administrator enforced)."
 expect "Current password:"
 send "$old_password\r"
-expect "(vmware-system-user@10.1.1.71) New password:"
+expect "(vmware-system-user@$host) New password:"
 send "$new_password\r"
-expect "(vmware-system-user@10.1.1.71) Retype new password:"
+expect "(vmware-system-user@$host) Retype new password:"
 send "$new_password\r"
 expect {vmware-system-user@auto-a-8fpl5 [ ~ ]$}
 send "sudo -i\r"
