08b2f2dced6d283d8cad35671000603a47c979bf
Burke Azbill <burkeazbill-bc@users.noreply.github.com>
2026-04-02 13:03:07 -0400

    Fix authentication issue

--- Tools/doupdate.sh
+++ Tools/doupdate.sh
@@ -11,16 +11,12 @@
 # Version 1.0 code:
 ###############################################################################
 WATCH_DIR="/mnt/manager"
+export KUBECONFIG=/etc/kubernetes/admin.conf
 # WATCH_INTERVAL=5  # seconds between checks
 logfile="/tmp/doupdate.log"
 log_message() {
-    echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1" >> ${logfile}
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1" >> "${logfile}"
     echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1"
-    
-    # Rotate log periodically (every 100 messages approximately)
-    if [ $((RANDOM % 100)) -eq 0 ]; then
-        rotate_log
-    fi
 }
 
 # if the file /home/holuser/hol/Tools/holorouter/certsrv_proxy.py exists, then copy it to /tmp/holorouter/certsrv_proxy.py
@@ -28,7 +24,7 @@ if [ -f "${WATCH_DIR}/certsrv_proxy.py" ]; then
     log_message "Found certsrv_proxy.py, processing" "${logfile}"
     cp -f "${WATCH_DIR}/certsrv_proxy.py" "/root/certsrv-proxy/certsrv_proxy.py"
     log_message "Restarting certsrv-proxy pod..."
-    kubectl -s https://192.168.0.2:6443 delete pod -n default -l app=certsrv-proxy > "${logfile}" 2>&1
+    kubectl delete pod -n default -l app=certsrv-proxy >> "${logfile}" 2>&1
     sleep 10
     # Make sure that the pod is running and age is less than 30 seconds
     if kubectl get pod -n default -l app=certsrv-proxy -o jsonpath='{.items[0].status.containerStatuses[0].restartCount}' | grep -q 0; then
